f398db29af2d015ed816950dcba39dfff4269eee
RobustGuava <147380829+RobustGuava@users.noreply.github.com>
2023-12-17 18:15:12 +0000

    Implemented facts search from lectures

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('client'));
+app.use(express.static("client"));
+
+const facts = [
+    { text: "Played for St Kilda", tags: ["geography", "history"] },
+    { text: "Was born in 1929", tags: ["history"] },
+    { text: "Is not famous for cookery", tags: ["friday"] }
+];
+
+app.get("/fact", function (request, response) {
+    const factNumber = parseInt(request.query.n);
+    response.send(facts[factNumber].text);
+});
+
+app.get("/facts/", function (request, response) {
+    const tag = request.query.tag;
+    const results = [];
+    for (const fact of facts) {
+        if (fact.tags.includes(tag)) {
+            results.push(fact.text);
+        }
+    }
+    response.send(results);
+});
+
+app.get("/tags", function (request, response) {
+    let tags = [];
+    for (const fact of facts) {
+        tags = tags.concat(fact.tags);
+    }
+    const tagSet = new Set(tags);
+    response.send([...tagSet]);
+});
 
 app.listen(8080);
