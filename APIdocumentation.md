21b78180f4fde40c2be90efa660d1d3c7adb6757
RobustGuava <147380829+RobustGuava@users.noreply.github.com>
2024-01-25 02:41:28 +0000

    added navbar

---
+++ APIdocumentation.md
@@ -0,0 +1,173 @@
+# API Documentation
+
+## Topics
+
+-
+### GET /topics
+
+It allows you to get a list of the data of all the topics.
+
+Endpoint URL
+
+[http://127.0.0.1:8080/topics](http://127.0.0.1:8080/topics)
+
+Example response:
+
+[
+
+{
+
+title "Maths",
+
+desc: "This is a flashcard set for Maths",
+
+numFlashcards: 15
+
+},
+
+{
+
+title: "Computer Science",
+
+desc: "This is a flashcard set for Computer Science",
+
+numFlashcards: 24
+
+},
+
+{
+
+title: "Physics",
+
+desc: "This is a flashcard set for Physics",
+
+numFlashcards: 72
+
+}
+
+]
+
+-
+### GET /topic
+
+It allows you to get the data of a topic.
+
+Endpoint URL
+
+[http://127.0.0.1:8080/topic](http://127.0.0.1:8080/topic)
+
+Query
+
+| Name | Type | Description |
+| --- | --- | --- |
+| title | text | The name of the topic you want to get |
+
+Example response:
+
+{
+
+title "Maths",
+
+desc: "This is a flashcard set for Maths",
+
+numFlashcards: 15
+
+}
+
+-
+### POST /topic/new
+
+Allows you to add a new topic.
+
+Endpoint URL
+
+[http://127.0.0.1:8080/topic/new](http://127.0.0.1:8080/topic/new)
+
+JSON body parameters
+
+| Name | Type | Description |
+| --- | --- | --- |
+| new-title | text | The name of the topic that you want to add. |
+| new-desc | text | The text of the description you want to add |
+
+## Flashcards
+
+-
+### GET /flashcards
+
+Allows you to get a list of all the flashcards for a topic.
+
+Endpoint URL:
+
+[http://127.0.0.1/flashcards](http://127.0.0.1/flashcards)
+
+Query
+
+| Name | Type | Description |
+| --- | --- | --- |
+| title | text | The title of the flashcards you want to get. |
+
+Example response:
+
+[
+
+{
+
+"question": "What is 1 + 1?",
+
+"answer": "1 + 1 = 2"
+
+},
+
+{
+
+"question": "What is 1 x 1?",
+
+"answer": "1 x 1 = 1"
+
+}
+
+]
+
+-
+### GET /flashcard
+
+Allows you to get a single flashcard for a topic.
+
+Endpoint URL:
+
+[http://127.0.0.1/flashcard](http://127.0.0.1/flashcard)
+
+Query
+
+| Name | Type | Description |
+| --- | --- | --- |
+| title | text | The title of the flashcard you want to get. |
+| index | integer | The index of the flashcard you want to get. (starts from 0). |
+
+Example response
+
+{
+
+"question": "What is 1 + 1?",
+
+"answer": "1 + 1 = 2"
+
+}
+
+-
+### POST /flashcard/new
+
+Allows you to add a new flashcard.
+
+Endpoint URL:
+
+[http://127.0.0.1/flashcard/new](http://127.0.0.1/flashcard/new)
+
+JSON body parameters
+
+| Name | Type | Description |
+| --- | --- | --- |
+| new-title | text | The title of the flashcard you want to add |
+| question | text | The question you want to add |
+| answer | text | The answer you want to add |
--- client/topics.js
+++ client/topics.js
@@ -1,22 +1,22 @@
 const topicsForm = document.getElementById('topics-form');
 const newTopicForm = document.getElementById('new-topic-form');
 
-async function newTopic () {
+async function newTopic() {
     const formData = new FormData(newTopicForm);
     const dataJson = JSON.stringify(Object.fromEntries(formData.entries()));
     // send a fetch request (POST) with the data
 
     await fetch('http://127.0.0.1:8080/topic/new', {
-      method: 'POST',
-      // need to set headers to make sure the server knows to invoke the JSON parser
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: dataJson
+        method: 'POST',
+        // need to set headers to make sure the server knows to invoke the JSON parser
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: dataJson
     });
 }
 
-async function getTopics () {
+async function getTopics() {
     const response = await fetch('http://127.0.0.1:8080/topics');
     if (response.ok) {
         return response.json();
@@ -25,7 +25,17 @@ async function getTopics () {
     }
 }
 
-async function populateDropdown () {
+async function getTopic(title) {
+    const response = await fetch('http://127.0.0.1:8080/topic?title=' + title);
+    if (response.ok) {
+        return response.json();
+    } else {
+        console.log(response.status)
+        alert('Sorry you cannot type you have a 404');
+    }
+}
+
+async function populateDropdown() {
     try {
         const topics = await getTopics();
         let html = '<option value="">Select</option>\n';
@@ -43,26 +53,41 @@ async function populateDropdown () {
     }
 }
 
-async function fillMainMenu () {
-    try {
-        const topics = await getTopics();
-        let html = '';
+function addTopicToMenu(topic) {
+    html = ''
+    html += '<div class="gallery">\n';
+    html += `<h2>${topic.title}</h2>\n`;
+    html += `<p>flashcards: ${topic.flashcardsCount}</p>\n`;
+    html += `<p>${topic.desc}</p>\n`;
+    html += '<button type="button" class="btn btn-primary bottom" data-toggle="modal" data-target="#newFlashcardModal">Add flashcard</button>';
+    html += `<button type="button" class="btn btn-primary bottom" style="right: 10px" data-toggle="modal" data-target="#flashcardModal" onclick="loadFlashcards('${topic.title}')">Study set</button>`;
+    html += '</div>\n';
 
-        for (const topic of topics) {
-            html += '<div class="gallery">\n';
-            html += `<h2>${topic.title}</h2>\n`;
-            html += `<p>flashcards: ${topic.flashcardsCount}</p>\n`;
-            html += `<p>${topic.desc}</p>\n`;
-            html += '<button type="button" class="btn btn-primary bottom" data-toggle="modal" data-target="#newFlashcardModal">Add flashcard</button>';
-            html += `<button type="button" class="btn btn-primary bottom" style="right: 10px" data-toggle="modal" data-target="#flashcardModal" onclick="loadFlashcards('${topic.title}')">Study set</button>`;
-            html += '</div>\n';
-        }
+    document.getElementById('menu-container').innerHTML += html
+}
 
-        document.getElementById('menu-container').innerHTML = html;
-    } catch (e) {
-        alert(e);
+async function fillMainMenu() {
+    const topics = await getTopics();
+    document.getElementById('menu-container').innerHTML = '';
+    for (const topic of topics) {
+        addTopicToMenu(topic);
     }
 }
+
+let searchTopicForm = document.getElementById("search-topic-form")
+
+searchTopicForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    try {
+        let title = document.getElementById("search-title").value;
+        const topic = await getTopic(title);
+        document.getElementById('menu-container').innerHTML = '';
+        addTopicToMenu(topic);
+    } catch (e) {
+        console.log(error);
+        fillMainMenu();
+    }
+});
 
 fillMainMenu();
 populateDropdown();
